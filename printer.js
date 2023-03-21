138da6ffa2b9485f0da3afbb2cfd13c445b34520
Pawel Rosiek <rosiu978@gmail.com>
2023-03-22 00:20:39 +0100

    adding asyn func

--- printer.js
+++ printer.js
@@ -3,8 +3,11 @@ printer.init({
     type: 'epson',
     interface: '/dev/usb/lp0'
 })
-
+async function example() {
 const isConnected = await printer.isPrinterConnected();
 console.log('Printer connected:', isConnected);
 
 printer.printIn('Hello world')
+}
+
+example()
