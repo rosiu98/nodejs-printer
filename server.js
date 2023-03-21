65dd1ba1a0b29fe02fb5194198261482b5c3df4b
Pawel Rosiek <rosiu978@gmail.com>
2023-03-22 00:53:01 +0100

    adding

--- server.js
+++ server.js
@@ -6,11 +6,26 @@
 // const printer = new escpos.Printer(device)
 
 // printer.text('TEST 12342342')
-
+const SerialPort = require('serialport')
 const escpos = require('escpos');
-escpos.SerialPort = require('escpos-serialport');
+escpos.SerialPort = require('escpos-serialport')
 
-const serialDeviceOnLinux = new escpos.SerialPort('/dev/usb/lp0', {
+const options = { encoding: "GB18030" /* default */ }
+const device = new escpos.SerialPort('/dev/usb/lp0', {
   baudRate: 19200,
   stopBit: 1
 });
+
+const printer = new escpos.Printer(device, options);
+
+device.open(function (err) {
+    printer
+     .align("CT") // align center
+     .style("B") // font weight bold
+     .size(0.1, 0.1) // text size
+     .text("Your big test text")
+     .size(0.01, 0.01)
+     .text("Your small test text")
+     .cut()
+     .close();
+  })
