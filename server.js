76146c43d2afdffbf06cd1aab3c5f8df496a686b
Pawel Rosiek <rosiu978@gmail.com>
2023-03-22 00:13:27 +0100

    new way to print

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const escpos = require('escpos')
+// const escpos = require('escpos')
 
-escpos.SerialPort = require('escpos-serialport')
+// escpos.SerialPort = require('escpos-serialport')
 
-const device = new escpos.SerialPort('/dev/usb/lp0')
-const printer = new escpos.Printer(device)
+// const device = new escpos.SerialPort('/dev/usb/lp0')
+// const printer = new escpos.Printer(device)
 
-printer.text('TEST 12342342')
+// printer.text('TEST 12342342')
+
+const escpos = require('escpos');
+escpos.SerialPort = require('escpos-serialport');
+
+const serialDeviceOnLinux = new escpos.SerialPort('/dev/usb/lp0', {
+  baudRate: 19200,
+  stopBit: 1
+});
