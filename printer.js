65c5de573e4c9aad34e9df27c40eef2fcf93c4d7
Pawel Rosiek <rosiu978@gmail.com>
2023-03-22 00:23:02 +0100

    adding stuff

--- printer.js
+++ printer.js
@@ -1,13 +1,13 @@
 const printer = require("node-thermal-printer");
-printer.init({
+async function example() {
+const printer = new ThermalPrinter({
     type: 'epson',
     interface: '/dev/usb/lp0'
 })
-async function example() {
 const isConnected = await printer.isPrinterConnected();
 console.log('Printer connected:', isConnected);
 
 printer.printIn('Hello world')
-}
 
+}
 example()
