f2f982c539d3b5f8b9d5de75428bdb677d999ba7
Pawel Rosiek <rosiu978@gmail.com>
2023-03-21 23:22:42 +0100

    adding fix

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const escpos = require('escpos')
 
-// escpos.USB = require('escpos-usb')
+escpos.USB = require('escpos-usb')
 
 const device = new escpos.Serial('/dev/usb/lp0')
-
 const printer = new escpos.Printer(device)
 
 printer.text('TEST 12342342')
