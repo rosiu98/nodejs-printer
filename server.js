8d3902afdeb25e3b9ab84529b459a5cab79ab43a
Pawel Rosiek <rosiu978@gmail.com>
2023-03-21 23:13:18 +0100

    adding another way to print

---
+++ server.js
@@ -0,0 +1,9 @@
+const escpos = require('escpos')
+
+// escpos.USB = require('escpos-usb')
+
+const device = new escpos.Serial('/dev/usb/lp0')
+
+const printer = new escpos.Printer(device)
+
+printer.text('TEST 12342342')
